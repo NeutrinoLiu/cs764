5643ac712c7d30c2e9ce1ed6a750ab659fe220ee
Yiwei Chen <y77chen@gmail.com>
2023-12-06 22:51:53 -0600

    defined new tournament tree interface

---
+++ Tournament.h
@@ -0,0 +1,26 @@
+#include "common.h"
+#include "Plan.h"
+#include "Iterator.h"
+
+class TournamentTreePlan : public Plan
+{
+    friend class TournamentTreeIterator;
+public:
+    TournamentTreePlan(const std::vector<Iterator*>& iterators);
+    ~TournamentTreePlan();
+    Iterator* init() const;
+private:
+    std::vector<Iterator*> read_iterators; // iterators for reading chunks
+};
+
+class TournamentTreeIterator : public Iterator
+{
+public:
+    TournamentTreeIterator(TournamentTreePlan const * const plan);
+    ~TournamentTreeIterator();
+    bool next();
+    char* get();
+
+private:
+    TournamentTreePlan const * const _plan;
+};
--- tournament.h
+++
@@ -1,5 +0,0 @@
-# include "common.h"
-# include "Iterator.h"
-class TournamentTree : Plan{
-    TournamentTree(std::vector<Iterator> leaves);
-};
